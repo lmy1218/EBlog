dc0d7911a2436d4baaf88eab3fdde4747cd9a728
limingyang <limingyang@meb.com>
2020-12-18 15:21:40 +0800

    fix

--- src/main/java/com/lmy/eblog/mq/MqMessageHandler.java
+++ src/main/java/com/lmy/eblog/mq/MqMessageHandler.java
@@ -3,6 +3,7 @@ package com.lmy.eblog.mq;
 
 import com.lmy.eblog.config.RabbitConfig;
 import com.lmy.eblog.search.service.SearchService;
+import io.netty.handler.codec.MessageAggregationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -11,28 +12,34 @@ import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
-@RabbitListener(queues = RabbitConfig.es_queue)
 public class MqMessageHandler {
 
     @Autowired
     SearchService searchService;
 
+    @RabbitListener(queues = RabbitConfig.es_queue)
     @RabbitHandler
     public void handler(PostMqIndexMessage message) {
 
-        log.info("mq 收到一条消息： {}", message.toString());
+        try {
+            log.info("mq 收到一条消息： {}", message.toString());
 
-        switch (message.getType()) {
-            case PostMqIndexMessage.CREATE_OR_UPDATE:
-                searchService.createOrUpdateIndex(message);
-                break;
-            case PostMqIndexMessage.REMOVE:
-                searchService.removeIndex(message);
-                break;
-            default:
-                log.error("没找到对应的消息类型，请注意！！ --》 {}", message.toString());
-                break;
+            switch (message.getType()) {
+                case PostMqIndexMessage.CREATE_OR_UPDATE:
+                    searchService.createOrUpdateIndex(message);
+                    break;
+                case PostMqIndexMessage.REMOVE:
+                    searchService.removeIndex(message);
+                    break;
+                default:
+                    log.error("没找到对应的消息类型，请注意！！ --》 {}", message.toString());
+                    break;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new MessageAggregationException("消息消费失败");
         }
+
     }
 
 }
